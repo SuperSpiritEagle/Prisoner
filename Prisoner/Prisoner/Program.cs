460c9f846c8ee51b59d596c258111f9eb4178a9b
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2024-03-14 10:53:50 +0500

    PrintPrisoners
    Добавил метод PrintPrisoners(), убрал публичный сет у свойства.

--- Prisoner/Prisoner/Program.cs
+++ Prisoner/Prisoner/Program.cs
@@ -8,40 +8,33 @@ namespace Prisoner
     {
         static void Main(string[] args)
         {
-            List<Prisoner> prisoners = new List<Prisoner>();
-
-            prisoners.Add(new Prisoner("Иванов Иван Иванович", true));
-            prisoners.Add(new Prisoner("Петров Петр Петрович", false));
-
-            Console.WriteLine("Список заключенных");
-            foreach (var prisoner in prisoners)
+            List<Prisoner> prisoners = new List<Prisoner>
             {
-                Console.WriteLine(prisoner);
-            }
+                new Prisoner("Иванов Иван Иванович", true),
+                new Prisoner("Петров Петр Петрович", false)
+            };
+
+            Console.WriteLine("Список заключенных:");
+
+            Prisoner.PrintPrisoners(prisoners);
 
             var antiGovernmentPrisoners = prisoners.Where(prisoner => prisoner.IsAntiGovernmentCrime);
 
             foreach (var prisoner in antiGovernmentPrisoners)
             {
-                prisoner.IsReleased = true;
+                prisoner.Release();
             }
 
             var releasedPrisoners = prisoners.Where(prisoner => prisoner.IsReleased);
 
             Console.WriteLine("Освобождены заключенные:");
-            foreach (var prisoner in releasedPrisoners)
-            {
-                Console.WriteLine(prisoner);
-            }
+
+            Prisoner.PrintPrisoners(prisoners);
         }
     }
 
     public class Prisoner
     {
-        public string FullName { get; set; }
-        public bool IsAntiGovernmentCrime { get; set; }
-        public bool IsReleased { get; set; }
-
         public Prisoner(string fullName, bool isAntiGovernmentCrime)
         {
             FullName = fullName;
@@ -49,11 +42,28 @@ namespace Prisoner
             IsReleased = false;
         }
 
+        public string FullName { get; }
+        public bool IsAntiGovernmentCrime { get; }
+        public bool IsReleased { get; private set; }
+
+        public void Release()
+        {
+            IsReleased = true;
+        }
+
         public override string ToString()
         {
             string crimeDescription = IsAntiGovernmentCrime ? "совершил антиправительственное преступление" : "не совершил антиправительственное преступление";
             string releaseStatus = IsReleased ? "освобожден" : "в тюрьме";
             return $"ФИО: {FullName}, Заключенный {crimeDescription}, Статус: {releaseStatus}.";
+        }
+
+        static public void PrintPrisoners(IEnumerable<Prisoner> prisoners)
+        {
+            foreach (var prisoner in prisoners)
+            {
+                Console.WriteLine(prisoner);
+            }
         }
     }
 }
